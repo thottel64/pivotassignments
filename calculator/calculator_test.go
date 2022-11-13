f7edb03b6cf7dcd2b843e1d8e09477f2d0c0419f
thottel64 <thottel64@gmail.com>
2022-11-13 16:12:33 -0600

    updated test functions to be table-driven

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -8,6 +8,7 @@ import (
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 type testingvariables struct {
 	a               int
 	b               int
@@ -130,8 +131,49 @@ func TestDivide(t *testing.T) {
 	want := 4
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
+=======
+func TestFunctions(t *testing.T) {
+	testmap := map[string]struct {
+		a               int
+		b               int
+		desired         int
+		operation       func(int, int) int
+		divideoperation func(int, int) (int, error)
+		err             error
+	}{
+		"Add":          {a: 3, b: 2, desired: 5, operation: calculator.Add},
+		"Subtract":     {a: 10, b: 7, desired: 3, operation: calculator.Subtract},
+		"Multiply":     {a: 6, b: 4, desired: 24, operation: calculator.Multiply},
+		"Divide":       {a: 12, b: 3, desired: 4, divideoperation: calculator.Divide},
+		"DivideByZero": {a: 1, b: 0, desired: 0, divideoperation: calculator.Divide, err: calculator.DivideByZero{}},
+>>>>>>> d5428be (updated test functions to be table-driven)
 	}
+	for name, test := range testmap {
+		t.Run(name, func(t *testing.T) {
+			if test.operation != nil {
+				result := test.operation(test.a, test.b)
+				if result != test.desired {
+					t.Errorf("got %v, want %v", result, test.desired)
+					return
+				}
+			}
+			if test.divideoperation != nil {
+				result, err := test.divideoperation(test.a, test.b)
+				if err != test.err {
+					t.Errorf("Did not receieve expected error")
+					return
+				}
+				if test.err == nil && err != nil {
+					t.Errorf("received unexcpected error %q", err)
+					return
+				}
+				if test.desired != result {
+					t.Errorf("got %v, want %v", result, test.desired)
+					return
+				}
+			}
 
+<<<<<<< HEAD
 }
 
 func TestDivideByZero_Error(t *testing.T) {
@@ -142,5 +184,8 @@ func TestDivideByZero_Error(t *testing.T) {
 	if err != error(calculator.DivideByZero{}) {
 		t.Errorf("different erorr than expected given. Wanted %v, got %v", error(calculator.DivideByZero{}), err)
 >>>>>>> e1165fb (reworked calculator tests as instructed)
+=======
+		})
+>>>>>>> d5428be (updated test functions to be table-driven)
 	}
 }

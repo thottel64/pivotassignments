d5428be985059f0f5e81d0a1c378d3437d4846ff
thottel64 <thottel64@gmail.com>
2022-11-13 16:12:33 -0600

    updated test functions to be table-driven

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,45 +5,46 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	got := calculator.Add(3, 2)
-	want := 5
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+func TestFunctions(t *testing.T) {
+	testmap := map[string]struct {
+		a               int
+		b               int
+		desired         int
+		operation       func(int, int) int
+		divideoperation func(int, int) (int, error)
+		err             error
+	}{
+		"Add":          {a: 3, b: 2, desired: 5, operation: calculator.Add},
+		"Subtract":     {a: 10, b: 7, desired: 3, operation: calculator.Subtract},
+		"Multiply":     {a: 6, b: 4, desired: 24, operation: calculator.Multiply},
+		"Divide":       {a: 12, b: 3, desired: 4, divideoperation: calculator.Divide},
+		"DivideByZero": {a: 1, b: 0, desired: 0, divideoperation: calculator.Divide, err: calculator.DivideByZero{}},
 	}
-}
-func TestSubtract(t *testing.T) {
-	got := calculator.Subtract(10, 7)
-	want := 3
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-func TestMultiply(t *testing.T) {
-	got := calculator.Multiply(4, 6)
-	want := 24
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-func TestDivide(t *testing.T) {
-	got, err := calculator.Divide(12, 3)
-	if err != nil {
-		t.Errorf("Divide function is returning an error")
-	}
-	want := 4
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+	for name, test := range testmap {
+		t.Run(name, func(t *testing.T) {
+			if test.operation != nil {
+				result := test.operation(test.a, test.b)
+				if result != test.desired {
+					t.Errorf("got %v, want %v", result, test.desired)
+					return
+				}
+			}
+			if test.divideoperation != nil {
+				result, err := test.divideoperation(test.a, test.b)
+				if err != test.err {
+					t.Errorf("Did not receieve expected error")
+					return
+				}
+				if test.err == nil && err != nil {
+					t.Errorf("received unexcpected error %q", err)
+					return
+				}
+				if test.desired != result {
+					t.Errorf("got %v, want %v", result, test.desired)
+					return
+				}
+			}
 
-}
-
-func TestDivideByZero_Error(t *testing.T) {
-	got, err := calculator.Divide(1, 0)
-	if got != 0 || err == nil {
-		t.Errorf("calculator is not handling divide by zero error correctly, no error received")
-	}
-	if err != error(calculator.DivideByZero{}) {
-		t.Errorf("different erorr than expected given. Wanted %v, got %v", error(calculator.DivideByZero{}), err)
+		})
 	}
 }

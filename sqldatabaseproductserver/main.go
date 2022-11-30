cd79b57057090cd1c2a3791ea99654e7b1ed7c55
thottel64 <thottel64@gmail.com>
2022-11-30 14:42:20 -0600

    database product server commit

---
+++ sqldatabaseproductserver/main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+var products []Product
+
+func main() {
+	db, err := sql.Open("sqlite3", "products.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("error pinging database")
+		return
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/products", GetProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", GetProduct).Methods("GET")
+	r.HandleFunc("/products", AddProduct).Methods("POST")
+	r.HandleFunc("/products/{id}", UpdateProduct).Methods("PUT")
+	r.HandleFunc("/products/{id}", DeleteProduct).Methods("DELETE")
+	fmt.Println("Server is running")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+
+}
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "products.db")
+	if err != nil {
+		return
+	}
+	limit := r.FormValue("limit")
+	query := "SELECT * FROM products LIMIT " + limit + ";"
+	if limit == "" {
+		query = "SELECT * FROM products;"
+	}
+	result, err := db.Query(query)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer result.Close()
+	for result.Next() {
+		var product Product
+		err := result.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		products = append(products, product)
+	}
+
+	err = json.NewEncoder(w).Encode(products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetProduct(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "products.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	vars := mux.Vars(r)
+	stringid := vars["id"]
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	intid, err := strconv.Atoi(stringid)
+	if intid > 100 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	query := "SELECT * FROM products WHERE ID = " + stringid + ";"
+	result, err := db.Query(query)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer result.Close()
+	var product Product
+	for result.Next() {
+		err := result.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+	response, err := json.Marshal(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+func AddProduct(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "products.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	request, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	newproduct := Product{
+		ID:    0,
+		Name:  "",
+		Price: 0,
+	}
+
+	err = json.Unmarshal(request, &newproduct)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	query := "INSERT INTO products (ID, Name, Price) VALUES (" + string(newproduct.ID) + ",`" + newproduct.Name + "`," + string(newproduct.Price) + ");"
+	_, err = db.Exec(query)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+
+}
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "products.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	vars := mux.Vars(r)
+	stringid := vars["id"]
+	id, err := strconv.Atoi(stringid)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if id > 100 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	request, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	newproduct := Product{
+		ID:    0,
+		Name:  "",
+		Price: 0,
+	}
+
+	err = json.Unmarshal(request, &newproduct)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	query := "UPDATE products SET Name = `" + newproduct.Name + "`, Price = " + string(newproduct.Price) + " WHERE ID =" + stringid
+	_, err = db.Exec(query)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "products.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	vars := mux.Vars(r)
+	stringid := vars["id"]
+	id, err := strconv.Atoi(stringid)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if id > 100 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	query := "DELETE FROM products WHERE ID =" + stringid
+	_, err = db.Exec(query)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}

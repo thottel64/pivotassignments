9c51a930ae26095e8931bf886f106e4be75d8dbd
thottel64 <105885349+thottel64@users.noreply.github.com>
2022-12-12 18:14:00 -0600

    database product server commit (#8)
    * database product server commit
    
    * added global for db and removed global product slice as requested
    
    * changed folder name to seeder for simplicity
    
    * undid previous commit
    
    * changed all queries to prepared statements and removed global error
    
    * replaced Query in GetProduct func with QueryRow and removed loop
    
    * handled specified errors and removed excess code
    
    * removed more excessive code and fixed http returns
    
    * added if statement for error in AddProduct func
    
    * resolved issue with stmt.Exec in UpdateProduct func

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 )
---
+++ sqldatabaseproductserver/main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+var db *sql.DB
+
+func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "products.db")
+	if err != nil {
+		fmt.Println("error opening database")
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("error pinging database")
+		return
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/products", GetProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", GetProduct).Methods("GET")
+	r.HandleFunc("/products", AddProduct).Methods("POST")
+	r.HandleFunc("/products/{id}", UpdateProduct).Methods("PUT")
+	r.HandleFunc("/products/{id}", DeleteProduct).Methods("DELETE")
+	fmt.Println("Server is running")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+
+}
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	var products []Product
+	limit := r.FormValue("limit")
+	stmt, err := db.Prepare("SELECT * FROM products LIMIT ?;")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	result, err := stmt.Query(limit)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = stmt.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for result.Next() {
+		var product Product
+		err := result.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		products = append(products, product)
+	}
+
+	err = json.NewEncoder(w).Encode(products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	stringid := vars["id"]
+	intid, err := strconv.Atoi(stringid)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	stmt, err := db.Prepare("SELECT * FROM products WHERE ID =?;")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	result := stmt.QueryRow(intid)
+	err = stmt.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var product Product
+	err = result.Scan(&product.ID, &product.Name, &product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	response, err := json.Marshal(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+func AddProduct(w http.ResponseWriter, r *http.Request) {
+	request, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	newproduct := Product{}
+
+	err = json.Unmarshal(request, &newproduct)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	stmt, err := db.Prepare("INSERT INTO products (ID, Name, Price) VALUES (?,?,?);")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	_, err = stmt.Exec(newproduct.ID, newproduct.Name, newproduct.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+}
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	stringid := vars["id"]
+	id, err := strconv.Atoi(stringid)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	request, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	newproduct := Product{}
+
+	err = json.Unmarshal(request, &newproduct)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	stmt, err := db.Prepare("UPDATE products SET Name =?, Price =?, WHERE ID =?")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = stmt.Exec(newproduct.Name, newproduct.Price, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	stringid := vars["id"]
+	id, err := strconv.Atoi(stringid)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	stmt, err := db.Prepare("DELETE FROM products WHERE ID =?")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = stmt.Exec(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
